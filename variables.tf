3a000407b71ae5837d1d1bcb3031e5157b82d678
Shibra Amin <shibra@squareops.com>
2023-03-24 11:42:41 +0530

    initial

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "environment" {
+  description = "Environment identifier for the EKS cluster"
+  default     = ""
+  type        = string
+}
+
+variable "name" {
+  description = "Specify the name of the EKS cluster"
+  default     = ""
+  type        = string
+}
+
+variable "cluster_version" {
+  description = "Kubernetes <major>.<minor> version to use for the EKS cluster"
+  default     = "1.23"
+  type        = string
+}
+
+variable "cluster_endpoint_public_access" {
+  description = "Indicates whether or not the Amazon EKS public API server endpoint is enabled"
+  default     = true
+  type        = bool
+}
+
+variable "cluster_endpoint_public_access_cidrs" {
+  description = "List of CIDR blocks which can access the Amazon EKS public API server endpoint"
+  default     = [""]
+  type        = list(string)
+}
+
+variable "kms_key_arn" {
+  description = "KMS key to Encrypt EKS resources."
+  default     = ""
+  type        = string
+}
+
