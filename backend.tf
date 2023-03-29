7bdf9d2c637d64c77c0cd30b9108a62187da25cb
Shibra Amin <shibra@squareops.com>
2023-03-29 15:36:51 +0530

    add assume role

--- backend.tf
+++ backend.tf
@@ -1,6 +1,13 @@
 terraform {
   backend "s3" {
-    
+    config = {
+    bucket = "stage-243620629044"
+    dynamodb_table = "stage-lock-dynamodb-243620629044"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+    external_id = "cross-acc"
+    role_arn = "arn:aws:iam::243620629044:role/SKAFCAA-SKAFCAA"
+  }
   }
 }
 
